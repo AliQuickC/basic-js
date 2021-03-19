0a3bd7dd0c038bacac7a8f9483eea160205b0f83
AliQuickC <aleksandera9@gmail.com>
2021-03-19 14:53:23 +0300

    feat: implement solution tasks Dream team and What season?

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if ( !Array.isArray(members) ) { return false; } // if the input parameter is not an array
+  
+  return members.map( (item,i,arr) => {
+    if (typeof(item) !== "string") { return ''} // if Name is not a string
+    if(item[0] != ' ') {                        // the first character is not a space
+      return item[0].toUpperCase()  
+    } 
+    else { 
+      let name = item.split("") 
+      for (let i = 0; i < name.length; i++) { // looking for the first character of a string that is not a space
+        if(name[i] != ' ') {          
+          return name[i].toUpperCase() }
+      }
+    }
+  } ).sort().join("");
+
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  const months = [];
+  months[0] = 'winter'
+  months[1] = 'winter'
+  months[2] = 'spring'
+  months[3] = 'spring'
+  months[4] = 'spring'
+  months[5] = 'summer'
+  months[6] = 'summer'
+  months[7] = 'summer'
+  months[8] = 'autumn'
+  months[9] = 'autumn'
+  months[10] = 'autumn'
+  months[11] = 'winter'
+
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date)) {
+    throw ("variable is not an instance of a date object");
+  }
+
+  if (toString.call(date) == "[object Date]") {
+    return months[date.getMonth()];
+  } else {
+    throw ("Date object received in the wrong format");
+  }
 };
