606d6b01b96169cfcdea8d68a580cf79628d379c
AliQuickC <aleksandera9@gmail.com>
2021-03-19 17:16:32 +0300

    feat: implement solution task Tower of Hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+                                  // number of disks, in turns per hour
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  let numberOfTurns = (Math.pow(2, disksNumber) - 1); // minimum number of turns to solve the puzzle
+  let secondsToSolve = Math.floor(3600 / turnsSpeed * numberOfTurns ); // seconds to solve the puzzle at a given turnsSpeed
+  
+  return {turns: numberOfTurns, seconds: secondsToSolve};
 };
