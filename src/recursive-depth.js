2f698d0cd484d41ff4dec22cb99f86b3fc76dae2
AliQuickC <aleksandera9@gmail.com>
2021-03-20 22:32:38 +0300

    feat: implement solution task Recursive depth calculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  constructor() {
+    this.level = 0  // current depth level
+    this.maxLevel = 0 // depth record
+  }
+
+  calculateDepth(arr) {
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if (this.level == 0 && !Array.isArray(arr)) { // check before recursion starts, if the function parameter is not an array
+      return 0
+    }
+
+    this.level++ // depth one level down
+    this.maxLevel < this.level ? this.maxLevel = this.level : this.maxLevel // checking for a new depth record
+
+    arr.forEach(element => {
+      if (Array.isArray(element)) {
+        this.calculateDepth(element) // recursive function call
+      }
+    });
+
+    this.level-- // depth one level up
+    if (this.level == 0) { // if we go up to level 0, clear the data before exiting the last recursion call      
+      let rezult = this.maxLevel
+      this.level = this.maxLevel = 0
+      return rezult
+    }
+    return this.maxLevel
   }
 };
