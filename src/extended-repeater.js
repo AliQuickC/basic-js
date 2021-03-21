fd4a7240aa9a1a9b0c4b22b18d7f220b5806f733
AliQuickC <aleksandera9@gmail.com>
2021-03-21 12:59:02 +0300

    feat: implement solution task Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  // throw new CustomError('Not implemented');
+  // remove line with error and write your code here  
+  let additionStr = '' // additional string
+  let rezStr = ''  // final string
+  
+  // form an additional string
+  if (options.addition !== undefined) {
+    options.addition = String(options.addition)
+    options.additionRepeatTimes =  options.additionRepeatTimes ?  options.additionRepeatTimes : 1  
+    options.additionSeparator = options.additionSeparator ? options.additionSeparator : '|'
+    
+    additionStr += options.addition
+    for (let i = 2; i <= options.additionRepeatTimes; i++) {
+      additionStr += options.additionSeparator + options.addition
+    }
+  }
+    
+  // form the final string from an additional string
+  strS = String(str)
+  options.repeatTimes =  options.repeatTimes ?  options.repeatTimes : 1
+  options.separator = options.separator ? options.separator : '+'
+  rezStr += strS + additionStr
+  for (let i = 2; i <= options.repeatTimes; i++) {
+    rezStr += options.separator + strS + additionStr
+  }
+    
+  return rezStr  
 };
   
